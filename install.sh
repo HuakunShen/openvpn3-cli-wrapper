dfe5475f792c4b3a48283cc36bd2a78a48272128
Huakun Shen <huakun.shen@huakunshen.com>
2022-01-28 14:50:14 -0500

    Update installation script and ovpn. ovpn now imports with persistence and added ls option which is equivalent to list option. And added -h options that's equivalent to --help option.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 curl -s https://api.github.com/repos/HuakunShen/openvpn3-cli-wrapper/releases/latest | grep download_url | cut -d '"' -f 4 | wget -i -
-
-sudo chmod +x ./ovpn
-sudo mv ./ovpn /usr/local/bin
+whichsudo=$(which sudo)
+if [ -z sudo ]; then
+	chmod +x ./ovpn
+	mv ./ovpn /usr/local/bin
+else
+	sudo chmod +x ./ovpn
+	sudo mv ./ovpn /usr/local/bin
+fi
